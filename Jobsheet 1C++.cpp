13a2f6ee00b986b490964da2afa175f11b2e5341
L. M. RYAS AMIN AKBAR <64558979+ryasakbar060@users.noreply.github.com>
2020-05-12 16:08:20 +0700

    Create Jobsheet 1C++.cpp
    Program array satu dimensi menampilkan NIM

---
+++ Jobsheet 1C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	cout<<"Nama : L.M.Ryas Amin Akbar "<<endl;
+	cout<<"NIM  : F1B019076"<<endl;
+	cout<<"Kelompok : 16 "<<endl<<endl;
+
+	int Nim_Mhsw [6]={5,6,7,8,10,12};
+	cout<<("Nim ke-1 = ", Nim_Mhsw[0])<<endl;
+	cout<<("Nim ke-2 = ", Nim_Mhsw[1])<<endl;
+	cout<<("Nim ke-3 = ", Nim_Mhsw[2])<<endl;
+	cout<<("Nim ke-4 = ", Nim_Mhsw[3])<<endl;
+	cout<<("Nim ke-5 = ", Nim_Mhsw[4])<<endl;
+	cout<<("Nim ke-6 = ", Nim_Mhsw[5])<<endl;
+}

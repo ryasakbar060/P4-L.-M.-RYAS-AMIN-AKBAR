cd8b9b139df13a50b9d352c15f9402a9825363d5
L. M. RYAS AMIN AKBAR <64558979+ryasakbar060@users.noreply.github.com>
2020-05-12 16:13:54 +0700

    Create Jobsheet 2C++.cpp
    Mengisi dan menampilkan elemen array index berdasarkan NIM

---
+++ Jobsheet 2C++.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	cout<<"Nama     : L.M.RYAS AMIN AKBAR"<<endl;
+	cout<<"Nim      : F1B019076"<<endl;
+	cout<<"Kelompok : 16"<<endl<<endl;
+	
+	int x [6];
+	
+	cout<<"Input Nilai ke-1 : ";
+	cin>>x[0];
+	
+	cout<<"Input Nilai ke-2 : ";
+	cin>>x[1];
+	
+	cout<<"Input Nilai ke-3 : ";
+	cin>>x[2];
+	
+	cout<<"Input Nilai ke-4 : ";
+	cin>>x[3];
+	
+	cout<<"Input Nilai ke-5 : ";
+	cin>>x[4];
+	
+	cout<<"Input Nilai ke-6 : ";
+	cin>>x[5];
+	
+	system("Pause");		
+}

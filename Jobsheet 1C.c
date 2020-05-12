71c6af3d32783795aad288ca340b502f6ec475b7
L. M. RYAS AMIN AKBAR <64558979+ryasakbar060@users.noreply.github.com>
2020-05-12 16:05:09 +0700

    Create Jobsheet 1C.c
    Program array satu dimensi menampilkan NIM

---
+++ Jobsheet 1C.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main()
+{
+	printf("Nama : L.M.RYAS AMIN AKBAR\n");
+	printf("NIM  : F1B019076\n");
+	printf("Kelompok : 16\n\n");
+	
+	int nim[6];
+	
+	nim[0]=1;
+	nim[1]=2;
+	nim[2]=3;
+	nim[3]=4;
+	nim[4]=5;
+	nim[5]=6;
+	
+	printf("Data ke-1 = %d\n",nim[0]);
+	printf("Data ke-2 = %d\n",nim[1]);
+	printf("Data ke-3 = %d\n",nim[2]);
+	printf("Data ke-4 = %d\n",nim[3]);
+	printf("Data ke-5 = %d\n",nim[4]);
+	printf("Data ke-6 = %d\n",nim[5]);
+	
+}

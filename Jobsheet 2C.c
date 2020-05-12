76e5727880086dc6e1ecdc2644b24f25805aa88a
L. M. RYAS AMIN AKBAR <64558979+ryasakbar060@users.noreply.github.com>
2020-05-12 16:11:13 +0700

    Create Jobsheet 2C.c
    Mengisi dan menampilkan elemen array index berdasarkan NIM

---
+++ Jobsheet 2C.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	printf("NAMA     : L,M.RYAS AMIN AKBAR\n");
+	printf("NIM      : F1B019076\n");
+	printf("KELOMPOK : 16\n\n");
+	
+	int a [6];
+	
+	printf("Masukkan nilai 1 :");
+	scanf("%d",a);
+	
+	printf("Masukkan nilai 2 :");
+	scanf("%d",a);
+	
+	printf("Masukkan nilai 3 :");
+	scanf("%d",a);
+	
+	printf("Masukkan nilai 4 :");
+	scanf("%d",a);
+	
+	printf("Masukkan nilai 5 :");
+	scanf("%d",a);
+	
+	printf("Masukkan nilai 6 :");
+	scanf("%d",a);
+	
+}
